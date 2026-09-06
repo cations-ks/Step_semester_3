09dfe5127965cc86a479e368a868ecd0379b4ded
cations-ks <krithi.sathish15@gmail.com>
2026-09-06 18:02:36 +0530

    Week 3 completed

---
+++ src/week_3_practice_problems/Applicant.java
@@ -0,0 +1,17 @@
+public class Applicant {
+
+    static int totalApplicants = 0;
+
+    public Applicant() {
+        totalApplicants++;
+    }
+
+    public static void main(String[] args) {
+
+        Applicant applicant1 = new Applicant();
+        Applicant applicant2 = new Applicant();
+        Applicant applicant3 = new Applicant();
+
+        System.out.println("Total applicants: " + Applicant.totalApplicants);
+    }
+}
